95569a24797dd5a7c71e2b6d0912b23c8de53c4d
Marcin Krystianc <marcin.krystianc@gmail.com>
2021-12-14 10:52:20 +0100

    Fix the exception check (#2)

--- src/NuExporter/NuGet/PublicPackagesProvider.cs
+++ src/NuExporter/NuGet/PublicPackagesProvider.cs
@@ -23,15 +23,16 @@ namespace NuExporter.NuGet
 
         public async Task<bool> IsPublicAsync(string packageId)
         {
-            var resource = await _repository.GetResourceAsync<MetadataResource>();
             try
             {
+                var resource = await _repository.GetResourceAsync<MetadataResource>();
                 var latestVersion = await resource.GetLatestVersion(packageId, true, false, _cacheContext,
                     NullLogger.Instance, CancellationToken.None);
 
                 return latestVersion != null;
             }
-            catch (FatalProtocolException e) when (e.InnerException is HttpRequestException httpRequestException)
+            catch (FatalProtocolException e) when (e.InnerException is FatalProtocolException innerException &&
+                                                   e.InnerException.InnerException is HttpRequestException httpRequestException)
             {
                 // Some NuGet sources throw 404 when asking for a non-existent package
                 if (httpRequestException.StatusCode == HttpStatusCode.NotFound)
