12da370eb1da83ca0a3e6dc8766d347f6d569606
Marcin Krystianc <marcin.krystianc@gmail.com>
2021-12-15 10:31:33 +0100

    remove unnecessary exception handling + v1.0.0

--- src/NuExporter/NuGet/PublicPackagesProvider.cs
+++ src/NuExporter/NuGet/PublicPackagesProvider.cs
@@ -23,23 +23,11 @@ namespace NuExporter.NuGet
 
         public async Task<bool> IsPublicAsync(string packageId)
         {
-            try
-            {
-                var resource = await _repository.GetResourceAsync<MetadataResource>();
-                var latestVersion = await resource.GetLatestVersion(packageId, true, false, _cacheContext,
-                    NullLogger.Instance, CancellationToken.None);
+            var resource = await _repository.GetResourceAsync<MetadataResource>();
+            var latestVersion = await resource.GetLatestVersion(packageId, true, false, _cacheContext,
+                NullLogger.Instance, CancellationToken.None);
 
-                return latestVersion != null;
-            }
-            catch (FatalProtocolException e) when (e.InnerException is FatalProtocolException innerException &&
-                                                   e.InnerException.InnerException is HttpRequestException httpRequestException)
-            {
-                // Some NuGet sources throw 404 when asking for a non-existent package
-                if (httpRequestException.StatusCode == HttpStatusCode.NotFound)
-                    return false;
-
-                throw;
-            }
+            return latestVersion != null;
         }
     }
 }
